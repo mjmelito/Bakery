b015c5eb112c1a44d7a59058a084bb2b4e43186c
Matt Melito <matt_melito@outlook.com>
2023-06-03 17:38:07 -0700

    Add initial Factory program directory with boilerplate controller code and Model files for Engineer, Machine, and FactoryContext.

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+
+}
---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+
+}
