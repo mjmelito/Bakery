1d5437c71dd1869fadda03a7152c6746445c853e
Matt Melito <matt_melito@outlook.com>
2023-06-04 06:09:15 -0700

    Add Tags Controller file.

---
+++ Factory/Controllers/TagsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class TagsController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public TagsController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Tags.ToList());
+    }
+
+    public ActionResult Details(int id)
+    {
+      Tag thisTag = _db.Tags
+          .Include(tag => tag.JoinEntities)
+          .ThenInclude(join => join.Engineer)
+          .FirstOrDefault(tag => tag.TagId == id);
+      return View(thisTag);
+    }
+  }
+}
