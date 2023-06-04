45ac7c2d2663305282d5636082ceee8c8ad1f539
Matt Melito <matt_melito@outlook.com>
2023-06-03 18:06:14 -0700

    Add Controller files for Engineer and Machine class.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
@@ -6,5 +7,77 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
 
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers
+                            .Include(engineer => engineer.Machine)
+                            .ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Categories, "MachineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      if (engineer.MachineId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisengineer = _db.Engineers
+                          .Include(engineer => engineer.Machine)
+                          .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisengineer);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      engineer thisengineer = _db.Engineers.FirstOrDefault(engineer => engineer.engineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Categories, "MachineId", "Name");
+      return View(thisengineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+      _db.Engineers.Update(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisengineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      _db.Engineers.Remove(thisEngineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
