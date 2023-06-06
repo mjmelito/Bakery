3b6fcf6928be293014c85fce948ec82754b8d044
Matt Melito <matt_melito@outlook.com>
2023-06-05 19:45:04 -0700

    Fix database error and change database name.

--- Factory/Migrations/20230606020901_Initial.cs
+++ Factory/Migrations/20230606020901_Initial.cs
@@ -28,6 +28,21 @@ namespace Factory.Migrations
                 .Annotation("MySql:CharSet", "utf8mb4");
 
             migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
                 name: "Engineers",
                 columns: table => new
                 {
@@ -49,16 +64,59 @@ namespace Factory.Migrations
                 })
                 .Annotation("MySql:CharSet", "utf8mb4");
 
+            migrationBuilder.CreateTable(
+                name: "EngineerTags",
+                columns: table => new
+                {
+                    EngineerTagId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EngineerId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineerTags", x => x.EngineerTagId);
+                    table.ForeignKey(
+                        name: "FK_EngineerTags_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EngineerTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
             migrationBuilder.CreateIndex(
                 name: "IX_Engineers_MachineId",
                 table: "Engineers",
                 column: "MachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EngineerTags_EngineerId",
+                table: "EngineerTags",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EngineerTags_TagId",
+                table: "EngineerTags",
+                column: "TagId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "EngineerTags");
+
+            migrationBuilder.DropTable(
                 name: "Engineers");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
 
             migrationBuilder.DropTable(
                 name: "Machines");
