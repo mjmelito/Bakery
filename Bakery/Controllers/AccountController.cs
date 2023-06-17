e82a8ed7483363396d240af610801dc8c21221c6
Matt Melito <matt_melito@outlook.com>
2023-06-17 15:32:53 -0700

    Add log out functionality.

--- Bakery/Controllers/AccountController.cs
+++ Bakery/Controllers/AccountController.cs
@@ -81,5 +81,12 @@ namespace Bakery.Controllers
         }
       }
     }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+    await _signInManager.SignOutAsync();
+    return RedirectToAction("Index");
+    }
   }
 }
