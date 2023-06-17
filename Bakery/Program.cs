043ba1b5e9f87e05a56fd79668c8acdf8a117334
Matt Melito <matt_melito@outlook.com>
2023-06-17 13:12:11 -0700

    Add using directive to Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Bakery.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Bakery
 {
